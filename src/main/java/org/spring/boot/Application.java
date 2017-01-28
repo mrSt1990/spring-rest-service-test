ae684be74260c80b792f9a2e0de85ecefed652af
Aleksey Stoyokha <astoyoha@mail.ru>
2017-01-28 23:35:33 +0300

    Start point added

--- src/main/java/org/spring/boot/Application.java
+++ src/main/java/org/spring/boot/Application.java
@@ -1,7 +1,15 @@
 package org.spring.boot;
 
+import org.spring.boot.controller.UsersController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by mr_St on 27.01.17.
  */
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
