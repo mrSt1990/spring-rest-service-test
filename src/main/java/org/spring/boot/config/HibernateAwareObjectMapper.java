1ad1762f952a387be850f80e93a0c36a143c2d03
Aleksey Stoyokha <astoyoha@mail.ru>
2017-01-28 23:40:11 +0300

    jackson-datatype-hibernate5 configuration classes added

--- src/main/java/org/spring/boot/config/HibernateAwareObjectMapper.java
+++ src/main/java/org/spring/boot/config/HibernateAwareObjectMapper.java
@@ -1,7 +1,15 @@
 package org.spring.boot.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.context.annotation.Bean;
+
 /**
  * Created by mr_St on 28.01.17.
  */
-public class HibernateAwareObjectMapper {
+public class HibernateAwareObjectMapper extends ObjectMapper {
+
+    public HibernateAwareObjectMapper() {
+        registerModule(new Hibernate5Module());
+    }
 }
--- src/main/java/org/spring/boot/config/JsonConfig.java
+++ src/main/java/org/spring/boot/config/JsonConfig.java
@@ -1,7 +1,22 @@
 package org.spring.boot.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
 /**
  * Created by mr_St on 28.01.17.
  */
+@Configuration
 public class JsonConfig {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter messageConverter() {
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        messageConverter.setObjectMapper(new HibernateAwareObjectMapper());
+        return messageConverter;
+    }
 }
