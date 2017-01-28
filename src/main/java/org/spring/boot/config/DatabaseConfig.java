bbfa447b7d6705d22fc81da17d341d5ff1983ab1
Aleksey Stoyokha <astoyoha@mail.ru>
2017-01-28 23:39:28 +0300

    added database configuration beans

--- src/main/java/org/spring/boot/config/DatabaseConfig.java
+++ src/main/java/org/spring/boot/config/DatabaseConfig.java
@@ -1,7 +1,59 @@
 package org.spring.boot.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import javax.sql.DataSource;
+import java.util.Properties;
+
 /**
  * Created by mr_St on 27.01.17.
  */
-public class DataBaseConfig {
+@Configuration
+public class DatabaseConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+        dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+        dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setDataSource(dataSource());
+        entityManagerFactory.setPackagesToScan(environment.getProperty("entityManager.packagesToScan"));
+        entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactory.setJpaProperties(buildHibernateProperties());
+        return entityManagerFactory;
+    }
+
+    private Properties buildHibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        hibernateProperties.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+        return hibernateProperties;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        return new JpaTransactionManager();
+    }
+
 }
+
+
+
+
